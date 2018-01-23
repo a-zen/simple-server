aa8b43a022bbae8b1fa22828c8945519441bc5c2
Zenger, Alexander <alexander.zenger@zuehlke.com>
2018-01-23 19:28:00 +0100

    added initial express app

---
+++ src/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.get('/ping', function (req, res) {
+  console.log('Received ping request from TODO');
+  res.send('pong');
+});
+
+app.get('/', function (req, res) {
+  console.log('Received root request from TODO');
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('started simple-dummy on port 3000');
+});
+
+
