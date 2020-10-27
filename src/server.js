ed7098e2f784fcf21907f906df9194c0e112fa59
alex <alex@zengers.de>
2020-10-27 23:02:37 +0100

    fixed linting issues

--- src/server.js
+++ src/server.js
@@ -1,26 +1,25 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
 
-var counter = 0;
+var counter = 0
 
 app.get('/ping', function (req, res) {
-  console.log('Received ping request from ' + req.connection.remoteAddress);
-  res.send('pong');
-});
+  console.log('Received ping request from ' + req.connection.remoteAddress)
+  res.send('pong')
+})
 
 app.get('/', function (req, res) {
-  counter++;
-  console.log('Received visitor request from ' + req.connection.remoteAddress
-    + ' number ' + counter);
-  res.send('Hello visitor number ' + counter);
-});
+  counter++
+  console.log('Received visitor request from ' + req.connection.remoteAddress +
+    ' number ' + counter)
+  res.send('Hello visitor number ' + counter)
+})
 
 app.listen(3000, function () {
-    console.log('started simple-dummy on port 3000');
-});
+  console.log('started simple-dummy on port 3000')
+})
 
 process.on('SIGTERM', function () {
-    console.info('Received SIGTERM, starting graceful shutdown');
-    process.exit(0);
-});
-
+  console.info('Received SIGTERM, starting graceful shutdown')
+  process.exit(0)
+})
